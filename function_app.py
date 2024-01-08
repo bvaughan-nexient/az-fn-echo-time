fb1f369ff2ba26c3d44ce0bd04d2c8bd2c1c002e
Ben Vaughan <ben.vaughan@nttdata.com>
2024-01-08 10:37:31 -0600

    Add JSON response to http_trigger function

--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 import logging
 import datetime
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -8,4 +9,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="utctime", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(f"{datetime.datetime.utcnow()}")
+    response = {}
+    response["timezone"] = "UTC"
+    response["timestamp"] = datetime.datetime.utcnow().isoformat()
+    response["format"] = "ISO 8601"
+    # return func.HttpResponse(f"{datetime.datetime.utcnow()}")
+    return func.HttpResponse(json.dumps(response), mimetype="application/json")
