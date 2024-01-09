fa229e5070b8522204128abbb44b83a5ce6a4a63
Ben Vaughan <111064685+bvaughan-nexient@users.noreply.github.com>
2024-01-09 10:36:58 -0600

    Refactor timestamp format in http_trigger function (#5)

--- function_app.py
+++ function_app.py
@@ -8,9 +8,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.function_name("echo_time")
 @app.route(route="utctime", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    # logging.info('Python HTTP trigger function processed a request.')
     response = {}
     response["timezone"] = "UTC"
-    response["timestamp"] = datetime.datetime.now(datetime.timezone.utc)[:-3] + 'Z'
+    response["timestamp"] = datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='milliseconds').replace('+00:00', 'Z')
     response["format"] = "ISO 8601"
     return func.HttpResponse(json.dumps(response), mimetype="application/json")
